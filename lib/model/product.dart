7df7c244269e4bf32275be4a99a9e7845b207ed2
sumitvarun <sumit.varun@rocketmail.com>
2022-05-30 14:02:04 +0530

    first commit

---
+++ lib/model/product.dart
@@ -0,0 +1,18 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite = false;
+
+  //constructor to initialize the class name.
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    required this.isFavorite,
+  });
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network('src'),
+    );
+  }
+}
