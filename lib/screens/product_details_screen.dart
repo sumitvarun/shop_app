e31354ad31581819b52626470079d98bb014fdcc
sumitvarun <sumit.varun@rocketmail.com>
2022-06-10 15:21:41 +0530

    Update

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -9,9 +11,12 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productId =
         ModalRoute.of(context)!.settings.arguments as String; //is the id!
+    final loadedProduct = Provider.of<Products>(context)
+        .items
+        .firstWhere((prod) => prod.id == productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
