0e04144dca60de938236ff8741e6666f12f8ac42
sumitvarun <sumit.varun@rocketmail.com>
2022-06-24 03:05:22 +0530

    Update

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/screens/product_details_screen.dart';
 
@@ -13,6 +14,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
    final product = Provider.of<Product> (context, listen:  false);
+   final cart = Provider.of<Cart> (context, listen:  false);
    //print('product rebuilds');
     return ClipRRect(
         borderRadius: BorderRadius.circular(18.0),
@@ -31,7 +33,9 @@ class ProductItem extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
             trailing: IconButton(
-                onPressed: () {}, icon: const Icon(Icons.shopping_cart)),
+                onPressed: () {
+                  cart.addItem(product.id, product.price, product.title,);
+                }, icon: const Icon(Icons.shopping_cart)),
           ),
           child: GestureDetector(
             onTap: (() {
