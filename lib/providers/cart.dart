476343a7cfac6b1b4dad6356532603f2851a5035
sumitvarun <sumit.varun@rocketmail.com>
2022-06-24 03:53:18 +0530

    Changes done

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -4,13 +4,13 @@ import 'package:provider/provider.dart';
 class CartItem {
   final String id;
   final String title;
- // final int quantity;
+  final int quantity;
   final double price;
 
   CartItem({
    required this.id, 
    required this.title, 
-  // required this.quantity, 
+   required this.quantity, 
    required this.price,
   });
 }
@@ -26,9 +26,9 @@ class Cart with ChangeNotifier {
   void addItem(String productId, double price, String title, ) {
     if(_items.containsKey(productId)) {
       // change quantity
-      _items.update(productId, (existingCartItem) => CartItem(id: existingCartItem.id, title: existingCartItem.title,  price: existingCartItem.price));
+      _items.update(productId, (existingCartItem) => CartItem(id: existingCartItem.id, title: existingCartItem.title,  price: existingCartItem.price, quantity: existingCartItem.quantity));
     } else {
-      _items.putIfAbsent(productId, () => CartItem(id: DateTime.now().toString(), title: title,  price: price));
+      _items.putIfAbsent(productId, () => CartItem(id: DateTime.now().toString(), title: title,  price: price, quantity: 1));
     }
   }
 
