8d6bfac3fb041c326105d37d6d46308f94ef2325
sumitvarun <sumit.varun@rocketmail.com>
2022-06-25 00:53:05 +0530

    Update

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 
-
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         appBar: AppBar(
+      appBar: AppBar(
         title: Text('Your Cart'),
       ),
       body: Column(
         children: <Widget>[
-          Card(margin: EdgeInsets.all(15), child: Padding(padding: EdgeInsets.all(8),child: Row(
-            children: [
-              
-            ],
-          ),),),
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                children: [],
+              ),
+            ),
+          ),
         ],
       ),
     );
