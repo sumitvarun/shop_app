1819b57be07e3c736f76367f7a0a3843864a17a8
sumitvarun <sumit.varun@rocketmail.com>
2022-06-26 10:34:13 +0530

    Making cart items dismissible

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -66,10 +66,12 @@ class CartScreen extends StatelessWidget {
           Expanded(
             child: ListView.builder(
               itemBuilder: (ctx, i) => CartItem(
-                  cart.items.values.toList()[i].id,
-                  cart.items.values.toList()[i].price,
-                  cart.items.values.toList()[i].quantity,
-                  cart.items.values.toList()[i].title),
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title,
+                cart.items.keys.toList()[i],
+              ),
               itemCount: cart.items.length,
             ),
           ),
