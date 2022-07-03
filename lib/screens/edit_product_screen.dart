00fe07bf3246c959828739e6955c8f79a0557b9b
sumitvarun <sumit.varun@rocketmail.com>
2022-07-04 00:15:55 +0530

    Added Multiline Input & Dispose Objects

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -11,6 +11,20 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _discriptionFocusNode = FocusNode();
+
+  // note : if you working with focusNode
+  //focusnode is stick with your memory
+  //  focusnode : you will have dispose them when your states get clear ,
+  // when that's  object get remove so when you leave that's  screen. because the focusnodes otherwise will stick around in memory.
+
+  @override
+  void dispose() {
+    //TODO : implement dispose
+    _priceFocusNode.dispose();
+    _discriptionFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,14 +50,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
               textInputAction: TextInputAction.next,
               keyboardType: TextInputType.number,
               focusNode: _priceFocusNode,
+              onFieldSubmitted: (_) {
+                //focusnode helps us to move user input from 1st textfield to second textfield.
+                FocusScope.of(context).requestFocus(_discriptionFocusNode);
+              },
             ),
             TextFormField(
               decoration: const InputDecoration(labelText: 'Discription'),
-              textInputAction: TextInputAction.next,
-              onFieldSubmitted: (_) {
-                //focusnode helps us to move user input from 1st textfield to second textfield.
-                FocusScope.of(context).requestFocus(_priceFocusNode);
-              },
+              maxLines: 3,
+              keyboardType: TextInputType.multiline,
+              focusNode: _discriptionFocusNode,
+              //  textInputAction: TextInputAction.next,
             ),
           ],
         )),
