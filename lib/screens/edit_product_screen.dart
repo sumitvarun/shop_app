60882c5473305ca8e800162b7c0872269b678377
sumitvarun <sumit.varun@rocketmail.com>
2022-07-03 23:55:05 +0530

    Added Form Input Focus

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,11 +10,13 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit Product'),
+        title: const Text('Edit Product'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -24,7 +26,25 @@ class _EditProductScreenState extends State<EditProductScreen> {
             TextFormField(
               decoration: InputDecoration(labelText: 'Title'),
               textInputAction: TextInputAction.next,
-            )
+              onFieldSubmitted: (_) {
+                //focusnode helps us to move user input from 1st textfield to second textfield.
+                FocusScope.of(context).requestFocus(_priceFocusNode);
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Price'),
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.number,
+              focusNode: _priceFocusNode,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Discription'),
+              textInputAction: TextInputAction.next,
+              onFieldSubmitted: (_) {
+                //focusnode helps us to move user input from 1st textfield to second textfield.
+                FocusScope.of(context).requestFocus(_priceFocusNode);
+              },
+            ),
           ],
         )),
       ),
