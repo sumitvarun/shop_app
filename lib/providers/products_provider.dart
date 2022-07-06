fc97b462eb3fc0bd2d50bf042a9abe9ac6cd7416
sumitvarun <sumit.varun@rocketmail.com>
2022-07-06 18:32:02 +0530

    Added Update Product feature

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -82,7 +82,7 @@ class Products with ChangeNotifier {
 
   void updateProduct(String id, Product newProduct) {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
-    if (prodIndex > 0) {
+    if (prodIndex >= 0) {
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
